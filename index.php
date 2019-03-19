5f879c3cdfcd923efac0b589e36b28a314b272e6
Batman <20029433+Stacced@users.noreply.github.com>
2019-03-19 22:06:49 +0100

    core: index / css basic stuff.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>HR Panel - Home</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<header>
+
+</header>
+</html>
