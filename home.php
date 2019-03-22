b630ac853affefdaf80246d8d722a423a47fc9df
Batman <20029433+stacced@users.noreply.github.com>
2019-03-22 22:47:42 +0100

    core/fix: home prep / random "1" below login form - fixed the random "1" that was appearing below login form (don't echo an include func) - no more error messages when loading form w/o data submitted - added navbar for home (wip)

--- home.php
+++ home.php
@@ -4,8 +4,6 @@ session_start();
 if (!isset($_SESSION['loggedin'])) {
     header("Location: index.php");
     die();
-} else {
-    echo $_SESSION['user'];
 }
 
 ?>
@@ -19,10 +17,32 @@ if (!isset($_SESSION['loggedin'])) {
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <a href="logout.php"><button class="btn btn-outline-secondary mr-sm-2" type="button">Logout</button></a>
-</nav>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">HR Panel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="home.php">Panel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Utilisateurs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Employés</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Départements</a>
+                </li>
+            </ul>
+        </div>
+        <a href="logout.php"><button class="btn btn-outline-secondary mr-sm-2" type="button">Logout</button></a>
+    </nav>
+</header>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
--- login.php
+++ login.php
@@ -11,35 +11,38 @@ $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 $pwd = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 $err = false;
 
-if (empty($email)) {
-    echo <<<EOF
+if (!empty($email) || !empty($pwd)) {
+    if (empty($email)) {
+        echo <<<EOF
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Erreur !</strong> Email non renseigné !
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
+    <strong>Erreur !</strong> Email non renseigné !
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
 </div>
 EOF;
-    $err = true;
-}
+        $err = true;
+    }
 
-if (empty($pwd)) {
-    echo <<<EOF
+    if (empty($pwd)) {
+        echo <<<EOF
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Erreur !</strong> Mot de passe non renseigné !
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
+    <strong>Erreur !</strong> Mot de passe non renseigné !
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
 </div>
 EOF;
+        $err = true;
+    }
+} else {
     $err = true;
 }
 
 if (!$err) {
     try {
-        $pdo = new PDO("mysql:dbname=" . $dbconf['dbname'] . ";host=" . $dbconf['hostname'] . ";charset=utf8", $dbconf['username'], $dbconf['password'] );
-    }
-    catch (PDOException $e) {
+        $pdo = new PDO("mysql:dbname=" . $dbconf['dbname'] . ";host=" . $dbconf['hostname'] . ";charset=utf8", $dbconf['username'], $dbconf['password']);
+    } catch (PDOException $e) {
         echo $e->getMessage();
         die();
     }
@@ -47,19 +50,22 @@ if (!$err) {
     $hashedPwd = strtoupper(hash('sha256', $pwd));
     $pdoStatement = $pdo->query("SELECT email, password FROM users WHERE email = '$email' AND password = '$hashedPwd'");
     $userData = $pdoStatement->fetch(PDO::FETCH_ASSOC);
+
     if (!empty($userData) && $pdoStatement->rowCount() == 1) {
-        echo "all good";
-        echo $userData['email'];
-        echo $userData['password'];
         header("Location: home.php");
         $_SESSION['loggedin'] = true;
         $_SESSION['user'] = $userData['email'];
     } else {
-        echo "bad | received multiple rows (row count => " . $pdoStatement->rowCount() . ")";
+        echo <<<EOF
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <strong>Erreur !</strong> Adresse email ou mot de passe incorrect
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+EOF;
     }
-
 }
-
 ?>
 
 <div id="form-box">
@@ -67,11 +73,13 @@ if (!$err) {
     <form action="" method="POST">
         <div class="form-group">
             <label for="idEmailInput">Adresse email</label>
-            <input type="email" name="email" class="form-control" id="idEmailInput" placeholder="Entrez votre email" required>
+            <input type="email" name="email" class="form-control" id="idEmailInput" placeholder="Entrez votre email"
+                   required value="<?= $email ?>">
         </div>
         <div class="form-group">
             <label for="idPasswordInput">Mot de passe</label>
-            <input type="password" name="password" class="form-control" id="idPasswordInput" placeholder="Entrez votre mot de passe" required>
+            <input type="password" name="password" class="form-control" id="idPasswordInput"
+                   placeholder="Entrez votre mot de passe" required>
         </div>
         <button type="submit" class="btn btn-primary">Se connecter</button>
     </form>
