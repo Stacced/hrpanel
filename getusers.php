77ceb59543b0a7eee3dfad6d4e22102091f1e207
Stacced <20029433+Stacced@users.noreply.github.com>
2019-04-10 10:59:26 +0200

    core: show users in database * that's it ? /shrug

---
+++ getusers.php
@@ -0,0 +1,37 @@
+<?php
+// Get database configuration
+$dbconf = parse_ini_file('dbsettings.ini');
+
+try {
+    // Database connection
+    $pdo = new PDO("mysql:dbnam=" . $dbconf['dbname'] . ";host=" . $dbconf['hostname'] . ";charset=utf8", $dbconf['username'], $dbconf['password']);
+
+    // Get users in array of arrays
+    $users = $pdo->query("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC);
+
+    // Convert enum value to text /
+    foreach ($users as &$user) {
+        switch ($user['permLevel']) {
+            case '1':
+                $user['permLevel'] = "Utilisateur";
+                break;
+            case '2':
+                $user['permLevel'] = "HR Mod";
+                break;
+            case '3':
+                $user['permLevel'] = "HR Admin";
+                break;
+        }
+    }
+    echo json_encode($users);
+} catch (PDOException $e) {
+    echo <<<EOF
+<div class="alert alert-danger alert-dismissible fade show" role="alert" id="idAlert">
+    <strong>Erreur !</strong> Connexion à la base de données impossible
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+EOF;
+    die();
+}
--- users.php
+++ users.php
@@ -55,18 +55,45 @@ if (empty($_SESSION['loggedin'])) {
         <th>Last successful login</th>
     </tr>
     </thead>
-    <tbody>
-    <tr>
-        <td>1</td>
-        <td>dindeleux@outlook.com</td>
-        <td>HR Admin</td>
-        <td>01/01/1970 - 12:00:00</td>
-    </tr>
+    <tbody id="idUsersData">
     </tbody>
 </table>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+$(document).ready(function() {
+    $.get({
+        url: 'getusers.php',
+        data: null,
+        success: function(result) {
+            let users = JSON.parse(result);
+
+            users.forEach(function(user) {
+                let userTr = document.createElement("tr");
+
+                let userId = document.createElement("td");
+                userId.innerHTML = user['idUser'];
+
+                let userEmail = document.createElement("td");
+                userEmail.innerHTML = user['email'];
+
+                let userPermLevel = document.createElement("td");
+                userPermLevel.innerHTML = user['permLevel'];
+
+                let userLastLogin = document.createElement("td");
+                userLastLogin.innerHTML = user['lastLogin'];
+
+                userTr.appendChild(userId);
+                userTr.appendChild(userEmail);
+                userTr.appendChild(userPermLevel);
+                userTr.appendChild(userLastLogin);
+                $('#idUsersData').append(userTr);
+            })
+        }
+    })
+})
+</script>
 </body>
 </html>
