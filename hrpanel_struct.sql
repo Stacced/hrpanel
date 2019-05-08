bab69faf14e87f9d4568908aa148da3cd8f0252d
Stacced <20029433+Stacced@users.noreply.github.com>
2019-05-08 11:38:20 +0200

    add: db struct (only) * no data !

--- hrpanel_struct.sql
+++ hrpanel_struct.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.1.28-MariaDB - mariadb.org binary distribution
+-- Server version:               5.7.17 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
--- HeidiSQL Version:             10.1.0.5464
+-- HeidiSQL Version:             10.1.0.5505
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,21 +16,42 @@
 CREATE DATABASE IF NOT EXISTS `hrpanel` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `hrpanel`;
 
+-- Dumping structure for table hrpanel.departments
+CREATE TABLE IF NOT EXISTS `departments` (
+  `idDept` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `deptName` text NOT NULL,
+  PRIMARY KEY (`idDept`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table hrpanel.employees
+CREATE TABLE IF NOT EXISTS `employees` (
+  `idEmployee` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `addrStreet` text,
+  `addrPostalCode` int(11) DEFAULT NULL,
+  `addrCity` text,
+  `idDept` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`idEmployee`),
+  KEY `fk_idDept` (`idDept`),
+  CONSTRAINT `fk_idDept` FOREIGN KEY (`idDept`) REFERENCES `departments` (`idDept`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
 -- Dumping structure for table hrpanel.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` text NOT NULL,
   `password` text NOT NULL,
   `permLevel` enum('1','2','3') NOT NULL DEFAULT '1',
   `lastLogin` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
--- Dumping data for table hrpanel.users: ~1 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`idUser`, `email`, `password`, `permLevel`, `lastLogin`) VALUES
-	(1, 'dindeleux@outlook.com', '016C756F0E615EF70CA05EB499A74D31C2DDEC7244760D59D792583C0410B36D', '3', '2019-05-07 20:55:26');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+-- Data exporting was unselected.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
