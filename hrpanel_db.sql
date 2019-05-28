db4bb4015895520dcea51ea66580b41bcebbdda9
Batman <20029433+Stacced@users.noreply.github.com>
2019-05-28 21:27:28 +0200

    db: reworked sql script * adds database structure * adds default database user * adds default site admin * adds default department

--- hrpanel_db.sql
+++ hrpanel_db.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.7.17 - MySQL Community Server (GPL)
+-- Server version:               10.1.28-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win32
--- HeidiSQL Version:             10.1.0.5505
+-- HeidiSQL Version:             10.1.0.5464
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -24,7 +24,6 @@ CREATE TABLE IF NOT EXISTS `departments` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
-
 -- Dumping structure for table hrpanel.employees
 CREATE TABLE IF NOT EXISTS `employees` (
   `idEmployee` int(11) NOT NULL AUTO_INCREMENT,
@@ -40,19 +39,27 @@ CREATE TABLE IF NOT EXISTS `employees` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
-
 -- Dumping structure for table hrpanel.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `idUser` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
   `email` text NOT NULL,
   `password` text NOT NULL,
   `permLevel` enum('1','2','3') NOT NULL DEFAULT '1',
   `lastLogin` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE USER IF NOT EXISTS 'adminpanel'@'localhost' IDENTIFIED BY 'adminpanelpassword';
+GRANT ALL PRIVILEGES ON hrpanel.* TO 'adminpanel'@'localhost';
+FLUSH PRIVILEGES;
+
+-- Default department
+INSERT INTO `hrpanel`.`departments` (`deptName`) VALUES ('Non-défini');
+
+-- Default admin user, password is "admin"
+INSERT INTO `hrpanel`.`users` (`email`, `password`, `permLevel`) VALUES ('admin@localhost.com', '8C6976E5B5410415BDE908BD4DEE15DFB167A9C873FC4BB8A81F6F2AB448A918', '3');
 
 -- Data exporting was unselected.
-
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
